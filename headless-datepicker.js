a40415789b5ed4529ba0d513c176dbb9eb5b0efb
Thorbjørn Jelgren <Thorbjrn Jelgren>
2017-04-30 22:17:13 +0200

    Write test while implementing the initial setup

--- headless-datepicker.js
+++ headless-datepicker.js
@@ -1,5 +1,59 @@
-module.exports = function(){
-    var t = this;
+var moment = require('moment')
 
-    return t;
+module.exports = function (options) {
+    var _selectedDates = []
+
+    var hdp = new Object();
+
+    options = options || {}
+
+    hdp.zeroBased = typeof options.zeroBased === 'undefined' ? true : options.zeroBased
+    hdp.minimumDate = options.minimumDate || null
+    hdp.maximumDate = options.maximumDate || null
+    hdp.disabledDates = options.disabledDates || []
+
+    hdp.localeSettings = options.localeSettings || {
+        firstDayOfWeek: 0,
+        dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+        monthNames: [ 'January','February','March','April','May','June','July','August','September','October','November','December' ],
+        monthNamesShort: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ]
+    }
+
+
+    var createSelectedDate = function(date){
+
+        // todo: fill in the blanks
+        var settings = hdp.localeSettings
+        var day = date.getDay()
+        var month = date.getMonth()
+        return {
+            date: date,
+            dateString: '',
+            dayName: settings.dayNames[day],
+            dayNameShort: settings.dayNamesShort[day],
+            dayNameMin: settings.dayNamesMin[day],
+            monthName: settings.monthNames[month],
+            monthNameShort: settings.monthNamesShort[month],
+            dayInWeek: 0,
+            dayInMonth: 0,
+            dayInYear: 0
+        }
+    }
+
+    
+    
+    hdp.setSelectedDates = function (dates) { 
+        _selectedDates = dates.map(function(date){ return createSelectedDate(date) }) 
+        
+    }
+
+    hdp.getSelectedDates = function(){ return _selectedDates }
+
+    hdp.setSelectedDate = function (date) { hdp.setSelectedDates([date]) }
+
+    hdp.getSelectedDate = function() { return !_selectedDates.length ? null : _selectedDates[_selectedDates.length - 1] }
+
+    return hdp
 }
--- headless-datepicker.spec.js
+++ headless-datepicker.spec.js
@@ -1,106 +1,203 @@
 
-const chai = require('chai')
-const assert = chai.assert
-const expect = chai.expect
+var chai = require('chai')
+var assert = chai.assert
+var expect = chai.expect
 
-const HeadlessDatepicker = require('./headless-datepicker')
+var HeadlessDatepicker = require('./headless-datepicker')
 
-describe('Headless datepicker', function () {
-	var headlessDatepicker;
+describe('Headless datepicker', () => {
+	var sut;
 
-	before(function () {
-		headlessDatepicker = new HeadlessDatepicker()
-	})
-	after(function () {
+	beforeEach(() => {
+		sut = new HeadlessDatepicker()
 	})
 
-	describe('Initialization', function () {
-		it('should be initialized', function () {
-			expect(headlessDatepicker).not.to.be.null
+	describe('Initialization', () => {
+		it('should be initialized', () => {
+			expect(sut).not.to.be.null
 		})
 
-		it.skip('should have zero-based months as default', function () {
-		})
-
-		it.skip('should override zero-based months by setting property', function () {
-		})
-	})
-
-	describe('Setting and getting dates', function () {
-		it.skip('should get selected date after setting a selected date', function () {
-		})
-
-		it.skip('should get array of selected dates after setting multiple dates', function () {
-		})
-
-		it.skip('should return latest selected date in getDate after setting multiple dates', function () {
-		})
-
-		it.skip('should get minimum date after setting a minimum date', function () {
-		})
-
-		it.skip('should get maximum date after setting a maximum date', function () {
-		})
-
-		it.skip('should get array of disabled dates after setting disabled date', function () {
-		})
-	})
-
-	describe('Localization and formatting', function () {
-		it.skip('should get default localization', function () {
-		})
-		
-		it.skip('should set and get localization options', function () {
-			/*
-			{
+		it('should have default options set', () => {
+			expect(sut.zeroBased).to.be.true
+			expect(sut.minimumDate).to.be.null
+			expect(sut.maximumDate).to.be.null
+			expect(sut.disabledDates).to.be.an('array')
+			expect(sut.localeSettings).to.deep.equal({
 				firstDayOfWeek: 0,
 				dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
 				dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-				dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"],
-				monthNames: [ "January","February","March","April","May","June","July","August","September","October","November","December" ],
-				monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+				dayNamesMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+				monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+				monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+			})
+		})
+
+		it('should override default options by setting options in contructor', () => {
+			var minimumDate = new Date(2017, 1, 10)
+			var maximumDate = new Date(2017, 1, 20)
+			var disabledDates = [new Date(2017, 1, 14), new Date(2017, 1, 15)]
+			var localeSettings = {
+				firstDayOfWeek: 1,
+				dayNames: ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"],
+				dayNamesShort: ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"],
+				dayNamesMin: ["Sø", "Ma", "Ti", "On", "To", "Fr", "Lø"],
+				monthNames: ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"],
+				monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"]
 			}
-			*/
+			sut = new HeadlessDatepicker({
+				zeroBased: false,
+				minimumDate: minimumDate,
+				maximumDate: maximumDate,
+				localeSettings: localeSettings,
+				disabledDates: disabledDates
+			})
+
+			expect(sut.zeroBased).to.be.false
+			expect(sut.minimumDate).to.deep.equal(minimumDate)
+			expect(sut.maximumDate).to.deep.equal(maximumDate)
+			expect(sut.disabledDates).to.deep.equal(disabledDates)
+			expect(sut.localeSettings).to.deep.equal(localeSettings)
 		})
 
-		
-		it.skip('should get date-string in correct moment-format', function () {
+		it('should support multiple objects', () => {
+			var sut2 = new HeadlessDatepicker({
+				zeroBased: false
+			})
+
+			expect(sut.zeroBased).to.be.true
+			expect(sut2.zeroBased).to.be.false
 		})
 
-		it.skip('should get array of date-strings in correct moment-format', function () {
+		it('should not pollute the global namespace', () => {
+			expect(() => { zeroBased }).to.throw()
 		})
 	})
 
-	describe('Getting date grid', function () {
-		it.skip('should return grid for a given month', function () {
+	describe('Setting and getting dates', () => {
+		it('should get selected date after setting a selected date', () => {
+			var testDate = new Date(2017, 5, 15)
+			sut.setSelectedDate(testDate)
+			var selectedDate = sut.getSelectedDate()
+
+			expect(selectedDate).not.to.be.null
+			expect(selectedDate.date).to.deep.equal(testDate)
 		})
 
-		it.skip('should show adjacent months', function () {
+		it('should get array of selected dates after setting multiple dates', () => {
+			var testDates = [new Date(2017, 5, 15), new Date(2017, 5, 16), new Date(2017, 5, 17)]
+			sut.setSelectedDates(testDates)
+			var selectedDates = sut.getSelectedDates()
+
+			expect(selectedDates).not.to.be.null
+			expect(selectedDates).to.be.an('array')
+			expect(selectedDates.length).to.equal(3)
+			expect(selectedDates.map((s) => { return s.date })).to.deep.equal(testDates)
 		})
 
-		it.skip('should not show adjacent months', function () {
+		it('should return latest selected date in getDate after setting multiple dates', () => {
+			var lastDate = new Date(2017, 5, 17)
+			var testDates = [new Date(2017, 5, 15), new Date(2017, 5, 16), lastDate]
+			sut.setSelectedDates(testDates)
+			var selectedDate = sut.getSelectedDate()
+
+			expect(selectedDate).not.to.be.null
+			expect(selectedDate.date).to.deep.equal(lastDate)
 		})
 
-		it.skip('should have selected dates marked', function () {
+		it.skip('should get array of disabled dates after setting disabled date', () => {
+		})
+	})
+
+	describe('The returned date object', () => {
+
+		describe('Day names', () => {
+			var weekDays = [new Date(2017, 0, 1), new Date(2017, 0, 2), new Date(2017, 0, 3), new Date(2017, 0, 4), new Date(2017, 0, 5), new Date(2017, 0, 6), new Date(2017, 0, 7)]
+			var dates;
+
+			beforeEach(() => {
+				sut.setSelectedDates(weekDays)
+				dates = sut.getSelectedDates()
+			})
+
+			it.skip('should get date-string in correct moment-format', () => {
+
+			})
+
+			it('should get correct day names', () => {
+				expect(dates.map(d => (d.dayName))).to.deep.equal(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])
+			})
+
+			it('should get correct day names in short', () => {
+				expect(dates.map(d => (d.dayNameShort))).to.deep.equal(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'])
+			})
+
+			it('should get correct day names in mini', () => {
+				expect(dates.map(d => (d.dayNameMin))).to.deep.equal(['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'])
+			})
 		})
 
-		it.skip('should have minimum date marked', function () {
+		describe('Month names', () => {
+			var months = [new Date(2017, 0, 1), new Date(2017, 1, 1), new Date(2017, 2, 1), new Date(2017, 3, 1), new Date(2017, 4, 1), new Date(2017, 5, 1), new Date(2017, 6, 1), new Date(2017, 7, 1), new Date(2017, 8, 1), new Date(2017, 9, 1), new Date(2017, 10, 1), new Date(2017, 11, 1)]
+
+			var dates;
+
+			beforeEach(() => {
+				sut.setSelectedDates(months)
+				dates = sut.getSelectedDates()
+			})
+
+			it('should get correct month names', () => {
+				expect(dates.map(d => (d.monthName))).to.deep.equal(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+			})
+
+			it('should get correct month names in short', () => {
+				expect(dates.map(d => (d.monthNameShort))).to.deep.equal([ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ])
+			})
+		})
+	})
+
+	describe('Getting date grid', () => {
+		it.skip('should return grid for a given month', () => {
 		})
 
-		it.skip('should have maximum date marked', function () {
+		it.skip('should show adjacent months', () => {
 		})
 
-		it.skip('should have disabled dates marked', function () {
+		it.skip('should not show adjacent months', () => {
 		})
 
-		it.skip('should respect zero based months', function () {
+		it.skip('should have selected dates marked', () => {
 		})
 
-		it.skip('should respect non-zero based months', function () {
+		it.skip('should have minimum date marked', () => {
 		})
 
-		it.skip('should respect first day of week', function () {
+		it.skip('should have minimum date included', () => {
 		})
-	})	
+
+		it.skip('should have dates below minimum as disabled', () => {
+		})
+
+		it.skip('should have maximum date marked', () => {
+		})
+
+		it.skip('should have maximum date included', () => {
+		})
+
+		it.skip('should have dates above maximum as disabled', () => {
+		})
+
+		it.skip('should have disabled dates marked', () => {
+		})
+
+		it.skip('should respect zero based months', () => {
+		})
+
+		it.skip('should respect non-zero based months', () => {
+		})
+
+		it.skip('should respect first day of week', () => {
+		})
+	})
 })
 
